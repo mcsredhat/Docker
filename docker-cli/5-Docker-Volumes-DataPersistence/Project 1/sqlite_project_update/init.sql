56d86fa784eb38865f93182638d1822e8aa224b4
assulai <farajassulai@gmail.com>
2025-06-14 16:49:26 +0200

    update the project files

---
+++ docker-cli/5-Docker-Volumes-DataPersistence/Project 1/sqlite_project_update/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+INSERT INTO users (id, name) VALUES (1, 'Alice');
+INSERT INTO users (id, name) VALUES (2, 'Ali');
+INSERT INTO users (id, name) VALUES (3, 'Faraj');
